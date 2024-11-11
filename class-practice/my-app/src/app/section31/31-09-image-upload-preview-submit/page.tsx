2d235f9be2a0f958755349e533b204c2a69efbe5
danggin <danggin.min@gmail.com>
2024-11-11 14:56:08 +0900

    ✅ update: 51일차 수업 코드 추가

---
+++ class-practice/my-app/src/app/section31/31-09-image-upload-preview-submit/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+
+const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+      images
+    }
+  }
+`;
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage() {
+  // const [imageUrl, setImageUrl] = useState("");
+  // const [file, setFile] = useState<File>();
+  const [imageUrls, setImageUrls] = useState(["", "", ""]);
+  const [files, setFiles] = useState<File[]>([]);
+
+  const [uploadFile] = useMutation(UPLOAD_FILE);
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  const onChangeFile = (index) => async (event) => {
+    const file = event.target.files?.[0]; // 배열로 들어오는 이유: <input type="file" multiple /> 일 때, 여러개 드래그 가능
+    if (file === undefined) return;
+    console.log(file);
+    console.log("파일까지 나옴");
+
+    // 2. 임시URL 생성 => (진짜URL - 다른 브라우저에서도 접근 가능)
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = (event) => {
+      console.log(event.target?.result); // 게시판에서 event.target.id를 쓰면 eslint가 잡았던 이유: event.target은 태그만을 가르키지 않음
+
+      if (typeof event.target?.result === "string") {
+        const tempUrls = [...imageUrls];
+        tempUrls[index] = event.target?.result;
+        setImageUrls(tempUrls);
+
+        const tempFiles = [...files];
+        tempFiles[index] = file;
+        setFiles(tempFiles);
+      }
+    };
+  };
+
+  const onClickSubmit = async () => {
+    // 1. uploadFile
+    // 1-1) 안좋은예제 - await를 매번 기다림 => for문 사용해도 마찬가지
+    const resultFile0 = await uploadFile({ variables: { file: files[0] } });
+    const resultFile1 = await uploadFile({ variables: { file: files[1] } });
+    const resultFile2 = await uploadFile({ variables: { file: files[2] } });
+    const url0 = resultFile0.data?.uploadFile.url;
+    const url1 = resultFile1.data?.uploadFile.url;
+    const url2 = resultFile2.data?.uploadFile.url;
+    const resultUrls = [url0, url1, url2];
+
+    // 2. createBoard
+    const result = await createBoard({
+      variables: {
+        createBoardInput: {
+          writer: "철수",
+          password: "1234",
+          title: "안녕하세요",
+          contents: "반갑습니다",
+          images: resultUrls, // [url0, url1, url2]
+        },
+      },
+    });
+    console.log(result);
+  };
+
+  return (
+    <>
+      <input type="file" onChange={onChangeFile(0)} />
+      <input type="file" onChange={onChangeFile(1)} />
+      <input type="file" onChange={onChangeFile(2)} />
+      <img src={imageUrls[0]} />
+      <img src={imageUrls[1]} />
+      <img src={imageUrls[2]} />
+      {/* <img src={`https://storage.googleapis.com/${imageUrl}`} /> */}
+
+      <button onClick={onClickSubmit}>게시글 등록하기</button>
+    </>
+  );
+}
---
+++ class-practice/my-app/src/app/section31/31-12-apollo-cache-state/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($mypage: Int) {
+    fetchBoards(page: $mypage) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($boardId: ID!) {
+    deleteBoard(boardId: $boardId)
+  }
+`;
+
+const CREATE_BOARD = gql`
+  mutation createBoardWithImage($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function ApolloStateCache() {
+  const { data } = useQuery(FETCH_BOARDS);
+  const [createBoard] = useMutation(CREATE_BOARD);
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  const onClickDelete = (boardId) => async () => {
+    await deleteBoard({
+      variables: { boardId },
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            fetchBoards: (prev, { readField }) => {
+              const deletedId = data.deleteBoard;
+
+              const filteredPrev = prev.filter(
+                (el) => readField("id", el) !== deletedId
+              );
+
+              return [...filteredPrev]; // 삭제된 id를 제외한 나머지만 return
+            },
+          },
+        });
+      },
+    });
+  };
+
+  const onClickSubmit = async () => {
+    await createBoard({
+      variables: {
+        createBoardInput: {
+          writer: "퍼글러",
+          title: "이빨괴물",
+          password: "1234",
+          contents: "우어어",
+        },
+      },
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            fetchBoards: (prev) => {
+              return [data.createBoard, ...prev];
+            },
+          },
+        });
+      },
+    });
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el: any) => (
+        <div key={el._id}>
+          <span style={{ marginRight: "10px" }}>{el.writer}</span>
+          <span>{el.title}</span>
+          <button onClick={onClickDelete(el._id)}>삭제하기</button>
+        </div>
+      ))}
+      <button onClick={onClickSubmit}>등록하기</button>
+    </div>
+  );
+}
